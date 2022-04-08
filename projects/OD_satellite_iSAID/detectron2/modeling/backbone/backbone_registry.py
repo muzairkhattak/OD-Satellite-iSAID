abf7a37decc3a946553dd1ca5fb825859b902c15
uzair khattak <mkhattak.bee17seecs@seecs.edu.pk>
2022-04-09 02:23:06 +0400

    minor fix

--- projects/OD_satellite_iSAID/detectron2/modeling/backbone/backbone_registry.py
+++ projects/OD_satellite_iSAID/detectron2/modeling/backbone/backbone_registry.py
@@ -32,31 +32,31 @@ model_urls = {
 }
 
 
-# @BACKBONE_REGISTRY.register()
-# def convnext_base(cfg, input_shape):
-#     # def convnext_base(pretrained=False, in_22k=False, **kwargs):
-#     model = ConvNeXt(cfg, depths=[3, 3, 27, 3], dims=[128, 256, 512, 1024])
-#     # Do required settings here for now, like all backbone specific
-#     # configurations
-#     pretrained = True
-#     in_22k = False # for now lets verfiy 1k pretrained backbone
-#     if pretrained:
-#         url = model_urls['convnext_base_22k'] if in_22k else model_urls['convnext_base_1k']
-#         checkpoint = torch.hub.load_state_dict_from_url(url=url, map_location="cpu")
-#         model.load_state_dict(checkpoint["model"])
-#     return model
-#
-# @BACKBONE_REGISTRY.register()
-# def build_convnext_fpn_backbone(cfg, input_shape):
-#     bottom_up = convnext_base(cfg, input_shape)
-#     in_features = cfg.MODEL.FPN.IN_FEATURES
-#     out_channels = cfg.MODEL.FPN.OUT_CHANNELS
-#     backbone = FPN(
-#         bottom_up=bottom_up,
-#         in_features=in_features,
-#         out_channels=out_channels,
-#         norm=cfg.MODEL.FPN.NORM,
-#         top_block=LastLevelMaxPool(),
-#         fuse_type=cfg.MODEL.FPN.FUSE_TYPE,
-#     )
-#     return backbone
+@BACKBONE_REGISTRY.register()
+def convnext_base(cfg, input_shape):
+    # def convnext_base(pretrained=False, in_22k=False, **kwargs):
+    model = ConvNeXt(cfg, depths=[3, 3, 27, 3], dims=[128, 256, 512, 1024])
+    # Do required settings here for now, like all backbone specific
+    # configurations
+    pretrained = True
+    in_22k = False # for now lets verfiy 1k pretrained backbone
+    if pretrained:
+        url = model_urls['convnext_base_22k'] if in_22k else model_urls['convnext_base_1k']
+        checkpoint = torch.hub.load_state_dict_from_url(url=url, map_location="cpu")
+        model.load_state_dict(checkpoint["model"])
+    return model
+
+@BACKBONE_REGISTRY.register()
+def build_convnext_fpn_backbone(cfg, input_shape):
+    bottom_up = convnext_base(cfg, input_shape)
+    in_features = cfg.MODEL.FPN.IN_FEATURES
+    out_channels = cfg.MODEL.FPN.OUT_CHANNELS
+    backbone = FPN(
+        bottom_up=bottom_up,
+        in_features=in_features,
+        out_channels=out_channels,
+        norm=cfg.MODEL.FPN.NORM,
+        top_block=LastLevelMaxPool(),
+        fuse_type=cfg.MODEL.FPN.FUSE_TYPE,
+    )
+    return backbone
